6a2f4f7e720eb40a571717fab9ac558213d89d65
Vlad <vlad3x@mail.ru>
2019-05-08 18:55:30 +0700

    base

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+function Article() {
+  const body = <section>Footer</section> 
+ return(
+      <div className="hello" style={{color:"red"}}>
+        <h2>title</h2>
+        <section>body</section> 
+        { body }
+        <h3 style={{color:"red"}}>
+         "creation date : "{ (new Date()).toDateString()}
+        </h3>
+      </div>
+    );
+}
+
+function App() {
+ return (
+    <div>
+        <h1>App Todor</h1>
+        <Article/>
+    </div>
+  );
+}
+
+render(<App/> ,document.getElementById('root'))
