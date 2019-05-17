ca52abadad919667f6191e46afb49e174a73c747
Vlad <vlad3x@mail.ru>
2019-05-17 20:04:34 +0700

    +

--- src/components/artos.js
+++ src/components/artos.js
@@ -1,21 +1,34 @@
 import React from 'react'
 
-// function Artos() {
-//   const body = <div>Artos here</div> 
-//  return(
-//       <p>Hello @</p>
-//     );
-// }
-// class Artos extends React.Component{
-// 	render(){
-// 		return(<p>Hello @</p>)
-			
-		
-// 	}
-// }
-const Artos = () => {
-  return(<p>Hello @</p>);
+const H1 = () =>{
+  return(<h1 style={{color:'blue'}}>Vlad</h1>)
+}
+const AboutMe = () => {
+  return(<p style={AboutMeCss}>Some text about me, i like food</p>)
+}
+const AboutMeCss = {
+  fontSize:"26px",
+  color: "#2EA8DB",
+  fontWeight: "600"
+}
+const Travel = () => {
+  return(
+  <ol>
+  <li>Europe</li>
+  <li>USA</li>
+  <li>Japan</li>
+  </ol>
+  )
 }
 
+const Artos = () => {
+  return(
+	<div>
+   <H1/>
+   <AboutMe/>
+   <Travel/>
+  </div>
+  	)
+}
 
 export default Artos
