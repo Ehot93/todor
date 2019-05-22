300fe72387cd9d4f3d833b741a21681ec7393940
Vlad <vlad3x@mail.ru>
2019-05-22 20:48:12 +0700

    next

---
+++ src/components/flags.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Flags = ()=>{
+	return(
+
+<form>
+    <label>
+      Тык <input type="checkbox" name="name" />
+    </label>
+    <label>
+      Мык <input type="checkbox" name="name" />
+    </label>
+    <label>
+      Пык <input type="checkbox" name="name" />
+    </label>
+    
+  </form>	
+	
+
+		)
+}
+export default Flags
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import {render} from 'react-dom'
 import Artos from './components/artos'
 import Footer from './components/footer'
 import CoolHeader from './components/header'
+import Flags from './components/flags'
 
 
 function Article() {
-  const body = <section>Content</section> 
+  const body = <section>Content here:</section> 
  return(
       <div className="hello" style={{color:"red"}}>
-        <h2>title</h2>
         <section>bodyy</section> 
         { body }
         <h3 style={{color:"red"}}>
@@ -25,6 +25,7 @@ function App() {
     <CoolHeader/>
     
         <h1>App Todor</h1>
+        <Flags/>
         <Artos/>
         <Article/>
         <Footer/>
