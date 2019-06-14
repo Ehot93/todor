8c6845c0229ccf358151265a6650cdb0d5292ccc
Vlad <vlad3x@mail.ru>
2019-06-15 01:31:16 +0700

    jokes 0.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import "./index.css"
-//import Artos from './components/artos'
+import Artos from './components/artos'
 import CoolHeader from './components/header'
 import TodoItem from './components/todoItem'
 import Apex from './components/apex'
